9de798fbc3f566ce30a1aee654183f014d10aa84
Laptop <admin@MacBook-Pro.local>
2025-10-05 20:03:17 +0300

    create UserList.jsx

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,3 +2,16 @@ import { useContext } from "react";
 
 import { AppContext } from "../context/AppContext";
 import UserProfile from "./UserProfile";
+
+export default function UserList() {
+    const { users } = useContext(AppContext);
+
+    return (
+        <div>
+            <h2>User List</h2>
+            {users.map((u) => (
+                <UserProfile key={u.id} user={u} />
+            ))}
+        </div>
+    );
+}
